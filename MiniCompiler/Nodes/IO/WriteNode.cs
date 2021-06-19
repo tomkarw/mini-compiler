2757123de7a82f6d9ad9bc8d7699109fade4078a
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-19 21:13:28 +0200

    [FEAT] write 'True' and 'False' for boolean values

--- MiniCompiler/Nodes/IO/WriteNode.cs
+++ MiniCompiler/Nodes/IO/WriteNode.cs
@@ -30,8 +30,17 @@ namespace MiniCompiler
                 }
                 case "i1":
                 {
-                    // TODO: Print 'True' or 'False'. I think llvm if statement is needed?
-                    sb.AppendLine($"call i32 (i8*, ...) @printf(i8* bitcast ([3 x i8]* @int_format to i8*), i1 %{id})");
+                    var trueLab = Context.GetNewId();
+                    var falseLab = Context.GetNewId();
+                    var endLab = Context.GetNewId();
+                    sb.AppendLine($"br i1 %{id}, label %{trueLab}, label %{falseLab}");
+                    sb.AppendLine($"{trueLab}:");
+                    sb.AppendLine("call i32 (i8*, ...) @printf(i8* bitcast ([5 x i8]* @true to i8*))");
+                    sb.AppendLine($"br label %{endLab}");
+                    sb.AppendLine($"{falseLab}:");
+                    sb.AppendLine("call i32 (i8*, ...) @printf(i8* bitcast ([6 x i8]* @false to i8*))");
+                    sb.AppendLine($"br label %{endLab}");
+                    sb.AppendLine($"{endLab}:");
                     break;
                 }
                 default:
