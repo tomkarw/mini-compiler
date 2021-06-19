c718bb71f8d7e463a8d840174deb40abcd173a0d
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-19 19:50:16 +0200

    [FEAT] if instruction

--- MiniCompiler/Nodes/Instructions/IfInstruction.cs
+++ MiniCompiler/Nodes/Instructions/IfInstruction.cs
@@ -6,19 +6,39 @@ namespace MiniCompiler
 
     {
         private readonly SyntaxNode _condition;
-        private readonly SyntaxNode _ifInstruction;
+        private readonly SyntaxNode _thenInstruction;
         private readonly SyntaxNode _elseInstruction;
 
-        public IfInstruction(SyntaxNode condition, SyntaxNode ifInstruction, SyntaxNode elseInstruction = null) : base(condition)
+        public IfInstruction(SyntaxNode condition, SyntaxNode thenInstruction, SyntaxNode elseInstruction = null) :
+            base(condition)
         {
             _condition = condition;
-            _ifInstruction = ifInstruction;
+            _thenInstruction = thenInstruction;
             _elseInstruction = elseInstruction;
         }
 
         public override string GenCode(ref StringBuilder sb)
         {
-            throw new System.NotImplementedException();
+            var t = _condition.GenCode(ref sb);
+
+            if (_condition.Type != "i1")
+            {
+                Context.AddError(_condition.Line,  _condition.Column, "If statement condition must return boolean");
+            }
+            
+            var trueLab = Context.GetNewId();
+            var falseLab = Context.GetNewId();
+            sb.AppendLine($"br i1 %{t}, label %{trueLab}, label %{falseLab}");
+            sb.AppendLine($"{trueLab}:");
+            _thenInstruction.GenCode(ref sb);
+            var endLab = Context.GetNewId();
+            sb.AppendLine($"br label %{endLab}");
+            sb.AppendLine($"{falseLab}:");
+            // else only if present
+            _elseInstruction?.GenCode(ref sb);
+            sb.AppendLine($"br label %{endLab}");
+            sb.AppendLine($"{endLab}:");
+            return null;
         }
     }
 }
