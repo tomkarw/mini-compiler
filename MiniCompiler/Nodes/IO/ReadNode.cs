16d96bee565cde0bd6ddf5d9f49e784bbb36a367
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-20 00:23:22 +0200

    [FEAT] read instruction

--- MiniCompiler/Nodes/IO/ReadNode.cs
+++ MiniCompiler/Nodes/IO/ReadNode.cs
@@ -15,8 +15,26 @@ namespace MiniCompiler
         public override string GenCode(ref StringBuilder sb)
         {
             var variable = Context.GetVariable(_id);
+            switch (variable.Type)
+            {
+                case "i32":
+                {
+                    sb.AppendLine($"call i32 (i8*, ...) @scanf(i8* bitcast ([3 x i8]* @int_format to i8*), i32* %{variable.Id})");
+                    break;
+                }
+                case "double":
+                { 
+                    sb.AppendLine( $"call i32 (i8*, ...) @scanf(i8* bitcast ([4 x i8]* @double_format to i8*), double* %{variable.Id})");
+                    break;
+                }
+                default:
+                {
+                    Context.AddError(_id.Line, _id.Line, $"Cannot read into type '{variable.Type}'");
+                    break;
+                }
+            }
 
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
