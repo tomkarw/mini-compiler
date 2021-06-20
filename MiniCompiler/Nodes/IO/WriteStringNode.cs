8f37a7239cbc1e175a6f170d6775e3a12c24d201
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-20 11:01:41 +0200

    [FIX] working '\' escaping

--- MiniCompiler/Nodes/IO/WriteStringNode.cs
+++ MiniCompiler/Nodes/IO/WriteStringNode.cs
@@ -18,18 +18,18 @@ namespace MiniCompiler
             s = s
                 // trim 
                 .Substring(1, s.Length - 2)
-                .Replace("\\", "#\\")
+                .Replace(@"\", @"#\")
                 // \n -> \0A
-                .Replace("#\\n", "\\0A")
+                .Replace(@"#\n", @"\0A")
                 // \" -> \22
-                .Replace("#\\\"", "\\22")
+                .Replace("#\\\"", @"\22")
                 // \\ -> \5C
-                .Replace("#\\\\", "\\5C")
-                .Replace("#\\", "");
+                .Replace(@"#\#\", @"\5C")
+                .Replace(@"#\", "");
             var cLength = s
-                .Replace("\\0A", "x")
-                .Replace("\\22", "x")
-                .Replace("\\5C", "x").Length + 1;
+                .Replace(@"\0A", "x")
+                .Replace(@"\22", "x")
+                .Replace(@"\5C", "x").Length + 1;
             sb.Insert(0, $"@{cId} = constant [{cLength} x i8] c\"{s}\\00\"\n");
             sb.AppendLine($"call i32 (i8*, ...) @printf(i8* bitcast ([{cLength} x i8]* @{cId} to i8*))");
             return null;
