db00a5f817c9ac00b0664cdc70cc302d8bfdf829
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-20 10:48:43 +0200

    better escaping, but still gives erros

--- MiniCompiler/Nodes/IO/WriteStringNode.cs
+++ MiniCompiler/Nodes/IO/WriteStringNode.cs
@@ -15,9 +15,21 @@ namespace MiniCompiler
         {
             var cId = Context.GetNewId();
             var s = String.Text;
-            s = s.Substring(1, s.Length - 2);
-            s = s.Replace("\\n", "\\0A"); // TODO: more replaces?
-            var cLength = s.Replace("\\0A", "x").Length + 1;
+            s = s
+                // trim 
+                .Substring(1, s.Length - 2)
+                .Replace("\\", "#\\")
+                // \n -> \0A
+                .Replace("#\\n", "\\0A")
+                // \" -> \22
+                .Replace("#\\\"", "\\22")
+                // \\ -> \5C
+                .Replace("#\\\\", "\\5C")
+                .Replace("#\\", "");
+            var cLength = s
+                .Replace("\\0A", "x")
+                .Replace("\\22", "x")
+                .Replace("\\5C", "x").Length + 1;
             sb.Insert(0, $"@{cId} = constant [{cLength} x i8] c\"{s}\\00\"\n");
             sb.AppendLine($"call i32 (i8*, ...) @printf(i8* bitcast ([{cLength} x i8]* @{cId} to i8*))");
             return null;
